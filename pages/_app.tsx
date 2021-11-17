475326833e2d9bdbaaf6d1501ba57dc357b168e4
Gabriel Araujo <gabriiel66@gmail.com>
2021-11-16 21:30:00 -0300

    course basement

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import 'styles/globals.css'
+import Navbar from 'components/Navbar'
+import { UserContext } from 'lib/context'
+import { useUserData } from 'lib/hooks'
+import { Toaster } from 'react-hot-toast'
+
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const userData = useUserData()
+
+  return (
+    <UserContext.Provider value={userData}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster />
+    </UserContext.Provider>
+  )
 }
 
 export default App
